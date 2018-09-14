f122eac9310202b0851db3232253a2ef719eb6aa
bing1zhi2 <bing1zhi2@163.com>
2018-09-14 23:00:34 +0800

    algo practice

---
+++ javaPractice/src/algoproblem/NLadder.java
@@ -0,0 +1,26 @@
+package algoproblem;
+
+/**
+ * Created by Administrator on 2018/9/14.
+ * @apiNote  N  1,2,3  上楼梯的所有可能情况
+ */
+public class NLadder {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.println(stepNums(a));
+    }
+
+    public static int stepNums(int ladderNum){
+        if(ladderNum<=0){
+            return 0;
+        }else if(ladderNum==1){
+            return 1;
+        }else if(ladderNum==2){
+            return 2;
+        }else if(ladderNum==3){
+            return 4;
+        }else {
+            return stepNums(ladderNum-3)+stepNums(ladderNum-2)+stepNums(ladderNum-1);
+        }
+    }
+}

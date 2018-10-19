6566c008f137422f58480302a9645282b3a5fc29
bing1zhi2 <bing1zhi2@163.com>
2018-10-19 11:00:57 +0800

    reverse str

---
+++ pyPractice/algoproblem/reverse.py
@@ -0,0 +1,35 @@
+def reverseString(s):
+    s = list(s)
+
+    end = len(s) - 1
+    begin = 0
+
+    while begin < end:
+        temp = s[begin]
+        s[begin] = s[end]
+        s[end] = temp
+        begin = begin + 1
+        end = end - 1
+
+    return "".join(s)
+
+
+r = reverseString('abcd')
+print(r)
+
+'''
+class SolutionClassic(object):
+    def reverseString(self, s):
+        r = list(s)
+        i, j  = 0, len(r) - 1
+        while i < j:
+            r[i], r[j] = r[j], r[i]
+            i += 1
+            j -= 1
+
+        return "".join(r)
+
+class SolutionPythonic(object):
+    def reverseString(self, s):
+        return s[::-1]
+'''

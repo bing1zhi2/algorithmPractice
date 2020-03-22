348d535a5b6e444a708a592c9d4661a10226fd90
bing1zhi2 <bing1zhi2@163.com>
2020-03-22 10:18:15 +0800

    leetcode 36 valid_sudoku

---
+++ pyPractice/algoproblem/valid_sudoku_36.py
@@ -0,0 +1,53 @@
+'''
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+A partially filled sudoku which is valid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+'''
+
+input_arr = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+print(input_arr)
+
+
+class Solution:
+    def valid_sudoku(self, arr):
+        rows = [{} for i in range(9)]
+        columns = [{} for i in range(9)]
+        boxes = [{} for i in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                num = arr[i][j]
+                # print(num)
+                if '.' != num:
+                    num = int(num)
+                    box_index = (i // 3) * 3 + j // 3
+                    # keep the current cell value
+                    rows[i][num] = rows[i].get(num, 0) + 1
+                    columns[j][num] = columns[j].get(num, 0) + 1
+                    boxes[box_index][num] = boxes[box_index].get(num, 0) + 1
+
+                    # check if this value has been already seen before
+                    if rows[i][num] > 1 or columns[j][num] > 1 or boxes[box_index][num] > 1:
+                        return False
+        return True
+
+
+s = Solution()
+result = s.valid_sudoku(input_arr)
+print(result)
